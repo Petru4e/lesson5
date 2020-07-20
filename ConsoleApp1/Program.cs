c0b912a27f70fe1dcc1286ec15c56898c18c3d46
Petru4e <petru4e85@mail.ru>
2020-07-20 20:55:06 +0300

    ex.1 and ex.2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-namespace ConsoleApp1
+
+namespace Login
 {
     class Program
     {
+        // Головинская Юлия Викторовна
+        // Создать программу, которая будет проверять корректность ввода логина.Корректным логином будет строка от 2 до 10 символов, 
+        // содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+        // а) без использования регулярных выражений;
+        //б) с использованием регулярных выражений.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region login
+
+            //Console.WriteLine("Введите логин: ");
+            //bool valid ;
+            //string login;
+            //string s1 = "Отлично.";
+            //string s2 = "Логин должен состоять из букв латинского алфавита или цифр(первый символ логина не может быть цифрой). Длина логина от 2 до 10 символов.";
+            //string s3;
+            //do
+            //{
+            //    login = Console.ReadLine();
+            //    valid = false;
+            //    if ((login.Length >= 2 && login.Length <= 10)
+            //        && (login[0] >= 'a' && login[0] <= 'z')
+            //       || (login[0] >= 'A' && login[0] <= 'Z'))
+            //    {
+            //        for (int i = 1; i < login.Length; i++)
+            //        {
+            //            if ((login[i] >= '0' && login[i] <= '9')
+            //                || (login[i] >= 'a' && login[i] <= 'z')
+            //                || (login[i] >= 'A' && login[i] <= 'Z'))
+            //                valid = true;
+            //            else valid = false;
+            //        }
+            //    }
+            //    else valid = false;
+
+            //    s3 = valid == true ? s1 : s2;
+            //    Console.WriteLine(s3);
+            //} while (valid==false);
+
+            #endregion
+            #region LoginRegex
+
+
+
+
+            //разобраться почему проходит вариант c повторяющейся буквой например: ghhhhhhhhhkkkkkkkkkkkkk
+
+            //Regex LoginValid = new Regex(@"^[a-zA-Z]+[0-9a-zA-Z]{1,9}$");
+            
+            //string login;
+            //do
+            //{
+            //    Console.WriteLine("Логин должен состоять из букв латинского алфавита \nили цифр(первый символ логина не может быть цифрой). \nДлина логина от 2 до 10 символов. \nВведите логин: ");
+            //    login = Console.ReadLine();
+                
+            //} while (LoginValid.IsMatch(login)==false);
+
+            //Console.WriteLine("Отлично!");
+
+
+
+
+            #endregion
+
         }
+
     }
 }
---
+++ Message/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Message
+{
+    class Program
+    {
+        //Разработать класс Message, содержащий следующие статические методы для обработки
+        //текста:
+        //а) Вывести только те слова сообщения, которые содержат не более n букв.
+        //б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+        //в) Найти самое длинное слово сообщения.
+        //г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+        //Продемонстрируйте работу программы на текстовом файле с вашей программой.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любую фразу: ");
+            string words = Console.ReadLine();
+            
+            Console.WriteLine($"Слова сообщения, которые содержат не более 5 букв: {FiveWords(words)}");
+            Console.WriteLine($"Сообщение без слов, которые заканчиваются на заданный символ(s): {NoWords(words)}");
+            Console.WriteLine($"Cамое длинное слово сообщения");
+            Console.WriteLine($"Строкa из самых длинных слов сообщения: ");
+
+        }
+        static string NoWords(string words)
+        {
+            Regex minWord = new Regex(@"s$");
+            string minWords = default;
+            foreach (Match match in minWord.Matches(words))
+            {
+                minWords = match.Value;
+            }
+            return minWords;
+        }
+        static string FiveWords(string words)
+        {
+            Regex minWord = new Regex(@".{1,9}");
+            string minWords = default;
+            foreach (Match match in minWord.Matches(words))
+            {
+                minWords = match.Value;
+            }
+            return minWords;
+        }
+
+    }
+}
